f7d0322a36b22eab44ccd147dba52c1d142f868e
Dipshikha Das <0802dipshikhadas@gmail.com>
2024-11-29 23:59:06 +0000

    updated files and basic template

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,16 +1,8 @@
-console.log('Welcome to My GitHub Website!');
-
-
-# Create folders and files
-for folder, files in structure.items():
-    folder_path = os.path.join(base_dir, folder)
-    os.makedirs(folder_path, exist_ok=True)
-    for file_name in files:
-        file_path = os.path.join(folder_path, file_name)
-        with open(file_path, "w") as file:
-            if file_name == "index.html":
-                file.write(index_html)
-            elif file_name == "styles.css":
-                file.write(styles_css)
-            elif file_name == "script.js":
-                file.write(script_js)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
